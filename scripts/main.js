de6d6e67a7aec4fe55cf929fc206fe05ce781b78
Ethan Jarrell <ethan.jarrell@gmail.com>
2017-06-14 19:42:50 -0400

    commit changes second commit

--- scripts/main.js
+++ scripts/main.js
@@ -10,54 +10,33 @@
 */
 
 
-
-
-var hand = [];
-
 function handValue (hand) {
-
   var acesInWaiting = [];
-
   var sum;
 
-for (var i = 0; i < hand.length-1; i++) {
-
-  //checking for a king, queen or Jack
-  if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
-
-    //if it is, changes the value to 10
-    hand === 10;
-  }
-
-//checking to see if it's an ace, and if so, pushing it to
-//aces in waiting
-
-    else if (hand[i] === "A") {
-    acesInWaiting.push(hand[i]);
-    hand[i] = 1;}
-
-
-  }
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+      hand[i] = 10;
+    } else if (hand[i] === "A") {
+      acesInWaiting.push(hand[i]);
+      hand[i] = 1;}
+    }
 
   for (var i = 0; i < hand.length; i++) {
     hand[i] = parseInt(hand[i]);
   }
+
   sum = (hand.reduce((a, b) => a + b, 0));
-
-
-
-  //if the sum of the entire hand is greater than 10, the value of the ace is 1, but if it's less than 10, all the aces have a value of 1 instead
-
   if (sum <= 10){
-    if (acesInWaiting.length = 1) {
+    if (acesInWaiting.length > 2) {
       acesInWaiting[i] = 11
-    } else if (acesInWaiting.length > 1) {
+    } else if (acesInWaiting.length < 2) {
       acesInWaiting[i] = 1;
     }
     acesInWaiting.reduce((a, b) => a + b, 0);
     sum += acesInWaiting;
     }
-return sum;
+  return sum;
 }
 
 /* -----  Hints ------
